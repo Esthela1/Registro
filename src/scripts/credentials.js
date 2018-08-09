ed9556ca3246bf4983c25e219781e3f0536c050e
Esthela1 <esthela_salas1212@hotmail.com>
2018-08-08 23:12:34 -0500

    inputs que guardan en firebase

--- src/scripts/credentials.js
+++ src/scripts/credentials.js
@@ -9,18 +9,31 @@ firebase.initializeApp({
 var db = firebase.firestore();
 
 //Add data
+function guardar(){
+  let name1 = document.getElementById("visitWorker").value;
+  let conpan = document.getElementById("company").value;
+  let name = document.getElementById("name").value;
+  let visitnt = document.getElementById("visit").value;
 
-db.collection("users").add({
-  first: "Ada",
-  last: "Lovelace",
-  born: 1815
-})
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
-});
+  db.collection("users").add({
+    visit: name1,
+    doing: conpan,
+    name2: name,
+    visiting: visitnt
+
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    document.getElementById("visitWorker").value = "";
+    document.getElementById("company").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("visit").value = "";
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  }); 
+}
+
 
 //
 
