d432962dedadc4cad46a63b2a19fdfb468f3b0e5
Esthela1 <esthela_salas1212@hotmail.com>
2018-08-08 21:18:37 -0500

    Firebase agregar

--- src/scripts/credentials.js
+++ src/scripts/credentials.js
@@ -1,10 +1,30 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAyS3BZ0O9m_GMM_asf4g77mEHFuNZn2iI",
-    authDomain: "registro-de-usuarios-8ca22.firebaseapp.com",
-    databaseURL: "https://registro-de-usuarios-8ca22.firebaseio.com",
-    projectId: "registro-de-usuarios-8ca22",
-    storageBucket: "registro-de-usuarios-8ca22.appspot.com",
-    messagingSenderId: "1017172818806"
-  };
-  firebase.initializeApp(config);
+firebase.initializeApp({
+  apiKey: "AIzaSyAyS3BZ0O9m_GMM_asf4g77mEHFuNZn2iI",
+  authDomain: "registro-de-usuarios-8ca22.firebaseapp.com",
+  projectId: "registro-de-usuarios-8ca22",
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+//Add data
+
+db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
+});
+
+//
+
+
+
+
+
