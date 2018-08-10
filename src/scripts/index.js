98c36b3cdaa9035143cae95de754feaa3341ab8a
BrisiaCastrejon <brissmonse@gmail.com>
2018-08-09 21:46:26 -0500

    Maquetación del login para administradores

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,18 @@
-alert('hola');
+singIn.addEventListener('click', event => {
+  buttonsInit.style.display = 'none';
+  formAdmin.style.display = 'block';  
+})
+openPage.addEventListener('click', event => {
+  if (adminMail.value == "worker@mail.com" && adminPassword.value == "123456") {
+    window.location.href = 'views/view2.html';
+  }else {
+    adminMail.value = '';
+    adminPassword.value = '';
+    alert('Discula las molestias NO eres un administrador');
+    buttonsInit.style.display = 'block';
+    formAdmin.style.display = 'none';
+  }
+})
+initFastAccess.addEventListener('click', event => {
+  window.location.href = 'views/view1.html'
+})
